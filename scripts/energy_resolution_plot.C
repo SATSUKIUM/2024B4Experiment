b16ebe1edc4feb9690f9e2866d3355c2a5a1abfd
Nirvana <yoshizawanaomichi111@gmail.com>
2024-12-06 18:39:24 +0900

    計算式変更,変数追加

--- scripts/energy_resolution_plot.C
+++ scripts/energy_resolution_plot.C
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <TGraph.h>
-#include <TGraphErrors.h>
+#include <TGraphErrors.h> 
 #include <TStyle.h> 
 #include <TString.h>
 #include <TCanvas.h>
@@ -13,16 +13,17 @@ using namespace std;
 void energy_resolution_plot(TString input_Folder = "./analysis/"){
     TString input_Filepath = Form("%senergy_resolution_test.txt",input_Folder.Data());
     ifstream ifs(input_Filepath);
-    double energy, ch, sigma_ch, sigma_energy;
+    double energy, ch, sigma_ch, sigma_gaus, sigma_gaus_energy;
 
     TCanvas* canvas = new TCanvas();
     TGraph* graph = new TGraph;
     int index_data = 0;
     Double_t R; //energy resolution
-    while(ifs >> energy >> ch >> sigma_ch){
-        sigma_energy = sigma_ch*29.06; //energy error in keV. for 11/20 RC7493 GSO data.
-        R = sigma_ch*2*sqrt(2*log(2))/ch;
-        graph->SetPoint(index_data, energy, R);
+    while(ifs >> energy >> ch >> sigma_ch >> sigma_gaus){
+        sigma_gaus_energy = sigma_gaus*51.4; //energy error in keV
+        R = sigma_gaus_energy*2*sqrt(2*log(2))/(51.4*ch-1.297);
+        graph->SetPoint(index_data, energy, R*100);
+        cout << index_data << endl;
         index_data++;
     }
     ifs.close();
