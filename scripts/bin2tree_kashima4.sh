9408f74aa25c0e14e9c4fab89b485c8ac5dc5200
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2024-12-12 17:49:47 +0900

    update binary2tree_kashimaC and bin2tree_kashima4.sh for new version of binary2tree macro.

--- scripts/bin2tree_kashima4.sh
+++ scripts/bin2tree_kashima4.sh
@@ -1,12 +1,12 @@
-# 8スレッドの並列処理でbinary2sato3.Cを回すようにしました。.rootファイルはROOT_FILESというフォルダに移送するようにした。
+# 8スレッドの並列処理でbinary2tree_kashima.Cを回すようにしました。.rootファイルはROOT_FILESというフォルダに移送するようにした。
 
 #!/bin/bash
 
 # .datファイルが格納されているフォルダー
-DAT_FOLDER="../data/20241129/RC7493/"
+DAT_FOLDER="../data/20241212/huruno1_m_0/"
 
 # ROOTスクリプトのパス
-ROOT_SCRIPT="binary2tree_sato4.C"
+ROOT_SCRIPT="binary2tree_kashima.C"
 
 # .rootファイルを移動させる共通のディレクトリ
 ROOT_DEST_FOLDER="${DAT_FOLDER}/ROOT_FILES"
@@ -27,7 +27,7 @@ find "$DAT_FOLDER" -type f -name "*.dat" | parallel -j 8 '
     echo "Processing $dat_file..."
     
     # ROOTコマンドを実行
-    root -b -l -q "$ROOT_SCRIPT(\"$dat_file\", 2, 0)" #("dat_file", Int1, Int2)のInt1はボード何台あるか
+    root -b -l -q "$ROOT_SCRIPT(\"$dat_file\")"
     
     # .rootファイルの生成を待ってから移動
     root_file="${dat_file}.root"  # .datファイルに対して .dat.root
