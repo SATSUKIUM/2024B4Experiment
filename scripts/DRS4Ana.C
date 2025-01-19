62dd4afc43308ff790e4e3585237f28d12647786
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-01-19 18:40:09 +0900

    NaI_peaksearch-> increase the resolution of pdf to be saved

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -1857,7 +1857,7 @@ Double_t DRS4Ana::NaI_peaksearch(Int_t iBoard = 0, Int_t iCh = 0, Double_t adcMi
     std::cout << "================================================================" << std::endl;
 
     //canvasの宣言など...
-    TCanvas *c1 = new TCanvas("c1", "Canvas", 1200, 900);
+    TCanvas *c1 = new TCanvas("c1", "Canvas", 1600, 1200);
     fH1ChargeIntegral = new TH1F("fH1ChargeIntegral", Form("%s:ch%d Charge Integral(for NaI) [%.1f,%.1f]", fRootFile.Data(), iCh, fChargeIntegralTmin, fChargeIntegralTmax), 500, adcMin, adcMax);
     fH1ChargeIntegral->SetXTitle("Voltage sum [V]");
     fH1ChargeIntegral->SetYTitle(Form("[counts] per %.2f V", (adcMax-adcMin)/500));
