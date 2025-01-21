0e0859462a94c46e5f6cafe3446c3f0cedf4d561
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-01-21 22:02:21 +0900

    debug: timebin problem

--- scripts/binary2tree_kashima.C
+++ scripts/binary2tree_kashima.C
@@ -498,9 +498,11 @@ int binary2tree_kashima(const Char_t *binaryDataFile = "../data/test001.dat", co
 
                         if(cumulative_time_buf >= 0){
                             time[iBoard][chID][icell] = cumulative_time_buf;
+                            printf("\ttime || %lf\n", time[iBoard][chID][icell]);
                         }
                         else{
                             time[iBoard][chID][icell] = cumulative_time_bin[iBoard][chID][1023] + cumulative_time_buf;
+                            printf("\ttime || %lf\n", time[iBoard][chID][icell]);
                         }
 
                         // // calculate time for this cell
