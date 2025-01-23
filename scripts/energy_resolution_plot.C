4bc507bf86cec7a262a94f151a54b52a4bbc2b28
yukai0903 <erika0903erika0903@gmail.com>
2025-01-23 17:32:20 +0900

    update

--- scripts/energy_resolution_plot.C
+++ scripts/energy_resolution_plot.C
@@ -9,9 +9,13 @@
 #include <TString.h>
 #include <TCanvas.h>
 #include <TF1.h>
+#include <iomanip>
+#include <chrono>
+#include <ctime> //時刻情報
+
 using namespace std;
-void energy_resolution_plot(TString input_Folder = "./analysis/"){
-    TString input_Filepath = Form("%senergy_resolution_test.txt",input_Folder.Data());
+void energy_resolution_plot(TString input_Folder = "./output/"){
+    TString input_Filepath = Form("%ss4_calib.txt",input_Folder.Data());
     ifstream ifs(input_Filepath);
     double energy, ch, sigma_ch, sigma_gaus, sigma_gaus_energy;
 
@@ -27,13 +31,15 @@ void energy_resolution_plot(TString input_Folder = "./analysis/"){
         index_data++;
     }
     ifs.close();
-    graph->SetTitle(";energy [keV];energy resolution (%)");
+    //graph->SetTitle(";energy [keV];energy resolution (%)");
+    graph->SetTitle(Form("energy calibration form %s;Energy [keV];energy resolution [%%]", input_Filepath.Data()));
+   
     graph->SetMarkerStyle(20);
     
     graph->Draw("ap");
     // graph->Fit("pol1");
-    TF1 *fitFunc = new TF1("fitFunc", "[0]/sqrt(x)", 10, 600);
-    fitFunc->SetParameter(0,30);
+    TF1 *fitFunc = new TF1("fitFunc", "[0]/sqrt(x)", 0, 1300);
+    fitFunc->SetParameter(0,300);
     graph->Fit(fitFunc);
     std::cout << "Fitting parameter [0]/sqrt(x) : " << fitFunc->GetParameter(0) << std::endl;
 
@@ -42,5 +48,14 @@ void energy_resolution_plot(TString input_Folder = "./analysis/"){
     gStyle->SetOptFit();
     canvas->Update();
 
-    canvas->SaveAs("./figure/energy_res.pdf");
+    TString filename_figure = "energy_res.pdf";
+    // 既にファイルが存在するか確認
+    Int_t index = 1;
+    while (gSystem->AccessPathName("./figure/" + filename_figure) == 0) {
+        // ファイルが存在する場合、ファイル名にインデックスを追加
+        filename_figure = Form("energy_res_%d.pdf", index);
+        index++;
+    }
+     canvas->SaveAs("./figure/" + filename_figure);
+
 }
