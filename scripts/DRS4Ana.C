4ff27d776ad93d6fb2d622e67dd0aae73803f8a3
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-01-25 14:22:54 +0900

    cleanup commentout

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -1354,7 +1354,6 @@ Double_t DRS4Ana::automated_peaksearch_SCA_mode(Int_t iBoard, Int_t iCh, Double_
     return (Double_t)counter;
 }
 
-
 Double_t DRS4Ana::GSO_peaksearch(Int_t iBoard = 0, Int_t iCh = 0, Double_t adcMin = 0, Double_t adcMax = 150.0, Int_t numPeaks = 10, Double_t fitRange = 2.0, Double_t adcTimeRange = 180.0)
 {
     Int_t append_Option = 1; //1 for not to overwrite the output.
@@ -1959,9 +1958,6 @@ Double_t DRS4Ana::NaI_peaksearch(Int_t iBoard = 0, Int_t iCh = 0, Double_t adcMi
 
     return (Double_t)counter;
 }
-
-
-
 
 Double_t DRS4Ana::peak_divided(Int_t iBoard = 0, Int_t iCh = 0, Double_t adcMin = 0.0, Double_t adcMax = 150.0, Double_t fitXmin = 0.0, Double_t fitXmax = 0.0, Double_t adcTimeRange = 180.0)
 {
