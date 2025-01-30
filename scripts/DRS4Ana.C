77ca98df6b314946bc7c7af995f5c36693aa8d5c
yukai0903 <erika0903erika0903@gmail.com>
2025-01-30 12:15:00 +0900

    commit

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -1664,11 +1664,11 @@ Double_t DRS4Ana::NaI_peaksearch(Int_t iBoard = 0, Int_t iCh = 0, Double_t adcMi
     for (Long64_t jentry = 0; jentry < nentries; jentry++)
     {
         fChain->GetEntry(jentry);
-        //timeCut_begin = fTime[iBoard][iCh][fDiscriCell[iBoard][iCh]] - 50;//トリガー時刻から-50 ns遡ってsum
-        //timeCut_end = fTime[iBoard][iCh][fDiscriCell[iBoard][iCh]] + adcTimeRange;//トリガー時刻から+adcTimeRange nsまでsum
+        timeCut_begin = fTime[iBoard][iCh][fDiscriCell[iBoard][iCh]] - 50;//トリガー時刻から-50 ns遡ってsum
+        timeCut_end = fTime[iBoard][iCh][fDiscriCell[iBoard][iCh]] + adcTimeRange;//トリガー時刻から+adcTimeRange nsまでsum
        
-        timeCut_begin = fTime[iBoard][iCh][0]; //時間カットなし
-        timeCut_end = fTime[iBoard][iCh][1023]; 
+       // timeCut_begin = fTime[iBoard][iCh][0]; //時間カットなし
+        //timeCut_end = fTime[iBoard][iCh][1023]; 
 
         chargeIntegral = GetChargeIntegral(iBoard, iCh, 20, timeCut_begin, timeCut_end);
         
