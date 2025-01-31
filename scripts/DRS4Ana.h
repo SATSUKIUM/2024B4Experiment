d2a44e9372d87c3f3da429eb8831adea10fa0569
Nirvana <yoshizawanaomichi111@gmail.com>
2025-01-31 19:38:52 +0900

    PlotSumEnergy_with_cutting

--- scripts/DRS4Ana.h
+++ scripts/DRS4Ana.h
@@ -125,6 +125,7 @@ public:
     virtual Double_t peak_divided(Int_t iBoard = 0, Int_t iCh = 0, Double_t adcMin = 0, Double_t adcMax = 150.0, Double_t fitXmin = 0.0, Double_t fitXmax = 0.0, Double_t adcTimeRange = 180.0);
     virtual void Load_EnergycalbData(TString key, Double_t p0[2][4], Double_t p0e[2][4], Double_t p1[2][4], Double_t p1e[2][4]);
     virtual Double_t semi_automated_spectrum_fitting(TString key_crystal = "NaI", Int_t iBoard = 0, Int_t iCh = 0, Double_t adcMin = 0, Double_t adcMax = 100);
+    virtual Double_t PlotSumEnergy_with_cutting(TString key = "0120", Int_t iBoard1 = 0, Int_t iCh1 = 0, Int_t iBoard2 = 1, Int_t iCh2 = 0, Double_t xmax = 1000);
 
 
 
