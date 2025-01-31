5df4592a0cd92201f81e422040b48f8a911321a6
Nirvana <yoshizawanaomichi111@gmail.com>
2025-01-31 15:48:54 +0900

    default argument

--- scripts/DRS4Ana.h
+++ scripts/DRS4Ana.h
@@ -112,7 +112,7 @@ public:
     virtual Double_t Output_MaxVoltage(Int_t how_many_boards = 1, Int_t iCh = 0);
     virtual Double_t Plot_2Dhist_energy_btwn_PMTs(TString key = "0120", TString key_Crystal_x = "NaI", TString key_Crystal_y = "NaI", Int_t x_iBoard = 0, Int_t x_iCh = 0, Int_t y_iBoard = 0, Int_t y_iCh = 1);
     virtual Double_t PlotEnergy(TString key, TString key_Crystal, Int_t iBoard, Int_t iCh, Double_t Vcut, Double_t xmin, Double_t xmax);
-    virtual Double_t PlotSumEnergy(TString key, TString key_Crystal1, Int_t iBoard1, Int_t iCh1, TString key_Crystal2, Int_t iBoard2, Int_t iCh2, Double_t Vcut, Double_t xmin, Double_t xmax);
+    virtual Double_t PlotSumEnergy(TString key = "0120", TString key_Crystal1 = "NaI", Int_t iBoard1 = 0, Int_t iCh1 = 0, TString key_Crystal2 = "NaI", Int_t iBoard2 = 0, Int_t iCh2 = 2, Double_t Vcut = 20, Double_t xmin = 0, Double_t xmax = 650);
     virtual Double_t PlotWavesWithThreshold(Int_t iBoard, Int_t iCh);
     virtual Double_t automated_peaksearch_SCA_mode(Int_t iBoard = 0, Int_t iCh = 0, Double_t Vcut = 20, Double_t xmin = 0.0, Double_t xmax = 50.0, Int_t numPeaks = 10, Double_t fitRange = 2.0);
     virtual Double_t GSO_peaksearch(Int_t iBoard = 0, Int_t iCh = 0, Double_t adcMin = 0, Double_t adcMax = 250.0, Int_t numPeaks = 10, Double_t fitRange = 3.0, Double_t spec_sigma = 5.0);
