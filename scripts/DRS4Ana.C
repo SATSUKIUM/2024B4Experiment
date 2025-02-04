e19101d603d6f31d1a370be1d5b39a753beed9d4
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-04 19:23:43 +0900

    resolution data

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -2733,11 +2733,9 @@ Double_t DRS4Ana::PlotSumEnergy_with_cutting(TString key_energy_calib = "0120", 
 
     //　エネルギーへの変換に必要なパラメータを取得
     Double_t p0[2][4], p0e[2][4], p1[2][4], p1e[2][4];
-    Load_EnergycalbData(key_energy_calib, p0, p0e, p1, p1e);
-
     // エネルギー分解能の1/sqrt(E)フィッティングパラメータ取得
-    Double_t p0_energy_res[2][4], p0e_energy_res[2][4];
-    Load_EnergyResData(key_energy_resolution, p0_energy_res, p0e_energy_res);
+    Double_t p0_res[2][4], p0e_res[2][4];
+    Load_EnergycalbData(key_energy_calib, p0, p0e, p1, p1e, p0_res, p0e_res);
 
     Double_t discriTime1, discriTime2, discriTime_S1, discriTime_A1;
     Double_t adcSum_timerange1, adcSum_timerange2;
