2a8266fdef834feee0ecce56d29373fd47126430
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-04 19:12:36 +0900

    LoadEnergyResdata->argument was not enough

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -146,24 +146,26 @@ void DRS4Ana::Load_EnergycalbData(TString key, Double_t p0[2][4], Double_t p0e[2
     ifs.close();
 }
 
-void DRS4Ana::Load_EnergyResData(TString key, Double_t p0[2][4]){
-    Double_t p0_buf;
+void DRS4Ana::Load_EnergyResData(TString key, Double_t p0[2][4], Double_t p0e[2][4]){
+    Double_t p0_buf, p0e_buf;
     TString calb_data_filepath = Form("./cfg/energy_res/%s/data.txt", key.Data());
     std::ifstream ifs(calb_data_filepath);
     Int_t line_index = 0;
-    while(ifs >> p0_buf){
+    while(ifs >> p0_buf >> p0e_buf){
         if(line_index == 8){
             break;
         }
         if(line_index < 4){
             p0[0][line_index] = p0_buf;
+            p0e[0][line_index] = p0e_buf;
             std::cout << Form("\tiBoard : 0, iCh : %d || energy resolution data loaded.\n", line_index % 4);
             std::cout << Form("\t\t%lf", p0_buf);
         }
         else if(line_index < 8){
             p0[1][line_index-4] = p0_buf;
+            p0e[1][line_index] = p0e_buf;
             std::cout << Form("\tiBoard : 1, iCh : %d || energy resolution data loaded.\n", line_index % 4);
-            std::cout << Form("\t\t%lf", p0_buf);
+            std::cout << Form("\t\t%lf %lf", p0_buf, p0e_buf);
         }
         line_index++;
     }
