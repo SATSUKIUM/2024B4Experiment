1f07282f0ddad653620348071ccd2debc5456540
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-05 15:39:42 +0900

    data loading update

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -139,6 +139,7 @@ void DRS4Ana::Load_EnergycalbData(TString key, Double_t p0[2][4], Double_t p0e[2
         }
         line_index++;
         if(line_index == 7){
+            line_index++;
             break;
         }
     }
@@ -2976,7 +2977,6 @@ Double_t DRS4Ana::Plot_2Dhist_energy_with_cut(TString key = "0120", TString key_
         DiscriTime_x = fTime[x_iBoard][x_iCh][fDiscriCell[x_iBoard][x_iCh]];
         DiscriTime_y = fTime[y_iBoard][y_iCh][fDiscriCell[y_iBoard][y_iCh]];
         if(100 < DiscriTime_y && DiscriTime_y < 1400){
-            printf("discritime x and y %f %f", DiscriTime_x, DiscriTime_y);
             x_charge_buf = -GetChargeIntegral(x_iBoard, x_iCh, 20, DiscriTime_x - 50, DiscriTime_x + adcSum_timerange_x);
             y_charge_buf = -GetChargeIntegral(y_iBoard, y_iCh, 20, DiscriTime_y - 50, DiscriTime_y + adcSum_timerange_y);
 
