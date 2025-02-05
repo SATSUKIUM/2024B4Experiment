e3dfa326119d7d53ac6b7690787317829b76443f
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-05 15:30:44 +0900

    data loading update

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -120,9 +120,7 @@ void DRS4Ana::Load_EnergycalbData(TString key, Double_t p0[2][4], Double_t p0e[2
     std::ifstream ifs(calb_data_filepath);
     Int_t line_index = 0;
     while(ifs >> p0_buf >> p0e_buf >> p1_buf >> p1e_buf){
-        if(line_index == 8){
-            break;
-        }
+        
         if(line_index < 4){
             p0[0][line_index] = p0_buf;
             p0e[0][line_index] = p0e_buf;
@@ -139,11 +137,11 @@ void DRS4Ana::Load_EnergycalbData(TString key, Double_t p0[2][4], Double_t p0e[2
             std::cout << Form("\tiBoard : 1, iCh : %d || energy calibration data loaded.\n", line_index % 4);
             std::cout << Form("\t\t%lf %lf %lf %lf\n", p0_buf, p1_buf, p0e_buf, p1e_buf);
         }
-        
         line_index++;
-        
+        if(line_index == 7){
+            break;
+        }
     }
-    line_index +=-1;
     Double_t dummy1,dummy2;
     while(ifs >> p0_res_buf >> p0e_res_buf >> dummy1 >> dummy2){
         if(line_index < 8){
