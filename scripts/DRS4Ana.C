0a9b77c515e889ab87c34b788535361c4a9ec53d
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-05 15:57:38 +0900

    data loading update

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -2951,6 +2951,7 @@ Double_t DRS4Ana::Plot_2Dhist_energy_with_cut(TString key = "0120", TString key_
     Double_t adcSum_timerange_x, adcSum_timerange_y;
     Double_t x_p0_res_buf = p0_res[x_iBoard][x_iCh];
     Double_t y_p0_res_buf = p0_res[y_iBoard][y_iCh];
+    printf("\n\n\ty_p0_res_buf %f\n\n", y_p0_res_buf);
     Double_t distance_from_511_line, x_distance_btwn_2points, y_distance_btwn_2points;
     if(key_Crystal_x == "NaI"){
         adcSum_timerange_x = 600;
@@ -2982,13 +2983,10 @@ Double_t DRS4Ana::Plot_2Dhist_energy_with_cut(TString key = "0120", TString key_
             x_energy = x_p0_buf + x_p1_buf*x_charge_buf;
             y_energy = y_p0_buf + y_p1_buf*y_charge_buf;
             x_error = x_p0_res_buf*sqrt(x_energy)/(2*sqrt(2*log(2)));
-            x_error = y_p0_res_buf*sqrt(y_energy)/(2*sqrt(2*log(2)));
-            printf("\ty_energy %f\n", y_energy);
-            // printf("\tx_error and y_error %f %f\n", x_error, y_error);
+            y_error = y_p0_res_buf*sqrt(y_energy)/(2*sqrt(2*log(2)));
             distance_from_511_line = pow((x_energy + y_energy - 511.0),2.0) / 2.0;
             x_distance_btwn_2points = pow((511.0+x_energy-y_energy)/2.0 - (511.0-y_energy), 2.0) + pow((511.0-x_energy+y_energy)/2.0 - y_energy, 2.0);
             y_distance_btwn_2points = pow((511.0+x_energy-y_energy)/2.0 - x_energy, 2.0) + pow((511.0-x_energy+y_energy)/2.0 - (511.0-x_energy), 2.0);
-            // printf("\tdistance %f %f %f\n", distance_from_511_line, x_distance_btwn_2points, y_distance_btwn_2points);
             
             if((pow(x_error,2.0) > (distance_from_511_line + x_distance_btwn_2points)) && (pow(y_error,2.0) > (distance_from_511_line + y_distance_btwn_2points))){
                 fH2Energy_PMTs->Fill(x_energy, y_energy);
