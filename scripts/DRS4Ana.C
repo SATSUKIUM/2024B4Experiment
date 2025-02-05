346aa81ac5adf179b3689c217b14da4fa79744c7
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-05 15:40:55 +0900

    data loading update

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -157,14 +157,14 @@ void DRS4Ana::Load_EnergycalbData(TString key, Double_t p0[2][4], Double_t p0e[2
             p0_res[0][line_index-8] = p0_res_buf;
             p0e_res[0][line_index-8] = p0e_res_buf;
             std::cout << Form("\tiBoard : 0, iCh : %d || energy resolution data loaded.\n", line_index % 4);
-            printf("\tline_index %d",line_index);
+            printf("\tline_index %d\n",line_index);
             std::cout << Form("\t\t%lf %lf\n", p0_res_buf, p0e_res_buf);
         }
         else if(line_index < 16){
             p0_res[1][line_index-12] = p0_res_buf;
             p0e_res[1][line_index-12] = p0e_res_buf;
             std::cout << Form("\tiBoard : 1, iCh : %d || energy resolution data loaded.\n", line_index % 4);
-            printf("\tline_index %d",line_index);
+            printf("\tline_index %d\n",line_index);
             std::cout << Form("\t\t%lf %lf\n", p0_res_buf, p0e_res_buf);
         }
         line_index++;
