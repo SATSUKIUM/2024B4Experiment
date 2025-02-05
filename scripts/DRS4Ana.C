9f27738bed4dae84421d5e210fb42e0ee1d4087f
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-05 14:53:34 +0900

    output message update

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -129,7 +129,7 @@ void DRS4Ana::Load_EnergycalbData(TString key, Double_t p0[2][4], Double_t p0e[2
             p1[0][line_index] = p1_buf;
             p1e[0][line_index] = p1e_buf;
             std::cout << Form("\tiBoard : 0, iCh : %d || energy calibration data loaded.\n", line_index % 4);
-            std::cout << Form("\t\t%lf %lf %lf %lf", p0_buf, p1_buf, p0e_buf, p1e_buf);
+            std::cout << Form("\t\t%lf %lf %lf %lf\n", p0_buf, p1_buf, p0e_buf, p1e_buf);
         }
         else if(line_index < 8){
             p0[1][line_index-4] = p0_buf;
@@ -137,7 +137,7 @@ void DRS4Ana::Load_EnergycalbData(TString key, Double_t p0[2][4], Double_t p0e[2
             p1[1][line_index-4] = p1_buf;
             p1e[1][line_index-4] = p1e_buf;
             std::cout << Form("\tiBoard : 1, iCh : %d || energy calibration data loaded.\n", line_index % 4);
-            std::cout << Form("\t\t%lf %lf %lf %lf", p0_buf, p1_buf, p0e_buf, p1e_buf);
+            std::cout << Form("\t\t%lf %lf %lf %lf\n", p0_buf, p1_buf, p0e_buf, p1e_buf);
         }
         
         line_index++;
@@ -151,13 +151,13 @@ void DRS4Ana::Load_EnergycalbData(TString key, Double_t p0[2][4], Double_t p0e[2
             p0_res[0][line_index-8] = p0_res_buf;
             p0e_res[0][line_index-8] = p0e_res_buf;
             std::cout << Form("\tiBoard : 0, iCh : %d || energy resolution data loaded.\n", line_index % 4);
-            std::cout << Form("\t\t%lf %lf (last 2 are dummy)", p0_res_buf, p0e_res_buf);
+            std::cout << Form("\t\t%lf %lf\n", p0_res_buf, p0e_res_buf);
         }
         else if(line_index < 16){
             p0_res[1][line_index-12] = p0_res_buf;
             p0e_res[1][line_index-12] = p0e_res_buf;
             std::cout << Form("\tiBoard : 1, iCh : %d || energy resolution data loaded.\n", line_index % 4);
-            std::cout << Form("\t\t%lf %lf (last 2 are dummy)", p0_res_buf, p0e_res_buf);
+            std::cout << Form("\t\t%lf %lf\n", p0_res_buf, p0e_res_buf);
         }
         line_index++;
     }
