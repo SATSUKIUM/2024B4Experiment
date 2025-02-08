3342ba05acd4d564686475477550be574766183a
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-08 13:58:51 +0900

    curve modified

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -79,11 +79,11 @@ void listChains(const TString key = "456"){
 
 Double_t error_curve_upper(Double_t* x, Double_t* par){
     // std::cout << 511.0 - x[0] + 0.01*(par[0] * sqrt(abs(511.0 - x[0])) / (2.0*sqrt(2.0*log(2.0))) + par[1] * sqrt(abs(x[0])) / (2.0*sqrt(2.0*log(2.0)))) << std::endl;
-    return(511.0 - x[0] + 0.01*(par[0] * sqrt(abs(511.0 - x[0])) / (2.0*sqrt(2.0*log(2.0))) + par[1] * sqrt(abs(x[0])) / (2.0*sqrt(2.0*log(2.0))))); //p0_res[0]はyのp0_res、p0_res[1]はxのp0_res
+    return(511.0 - x[0] + par[3]*0.01*(par[0] * sqrt(abs(511.0 - x[0])) / (2.0*sqrt(2.0*log(2.0))) + par[1] * sqrt(abs(x[0])) / (2.0*sqrt(2.0*log(2.0))))); //p0_res[0]はyのp0_res、p0_res[1]はxのp0_res
 }
 Double_t error_curve_lower(Double_t* x, Double_t* par){
     // std::cout << 511.0 - x[0] - 0.01*(par[0] * sqrt(abs(511.0 - x[0])) / (2.0*sqrt(2.0*log(2.0))) + par[1] * sqrt(abs(x[0])) / (2.0*sqrt(2.0*log(2.0)))) << std::endl;
-    return(511.0 - x[0] - 0.01*(par[0] * sqrt(abs(511.0 - x[0])) / (2.0*sqrt(2.0*log(2.0))) + par[1] * sqrt(abs(x[0])) / (2.0*sqrt(2.0*log(2.0))))); //par[0]はyのp0_res、par[1]はxのp0_res
+    return(511.0 - x[0] - par[3]*0.01*(par[0] * sqrt(abs(511.0 - x[0])) / (2.0*sqrt(2.0*log(2.0))) + par[1] * sqrt(abs(x[0])) / (2.0*sqrt(2.0*log(2.0))))); //par[0]はyのp0_res、par[1]はxのp0_res
 }
 
 void DRS4Ana::PlotADCSum(Int_t iBoard, Int_t iCh)
@@ -3587,7 +3587,7 @@ Double_t DRS4Ana::Plot_2Dhist_energy_with_cut2(TString key = "0120", TString key
 
         DiscriTime_x = fTime[x_iBoard][x_iCh][fDiscriCell[x_iBoard][x_iCh]];
         DiscriTime_y = fTime[y_iBoard][y_iCh][fDiscriCell[y_iBoard][y_iCh]];
-        if(100 < DiscriTime_y && DiscriTime_y < 1400){
+        if(150 < DiscriTime_y && DiscriTime_y < 250){
             x_charge_buf = -GetChargeIntegral(x_iBoard, x_iCh, 20, DiscriTime_x - 50, DiscriTime_x + adcSum_timerange_x);
             y_charge_buf = -GetChargeIntegral(y_iBoard, y_iCh, 20, DiscriTime_y - 50, DiscriTime_y + adcSum_timerange_y);
 
@@ -3612,12 +3612,14 @@ Double_t DRS4Ana::Plot_2Dhist_energy_with_cut2(TString key = "0120", TString key
 
                     // distance_from_511_line = pow((energy_S1 + energy_A1 - 511.0),2.0) / 2.0;
 
-                    if(abs(energy_A1 - (511.0 - energy_S1)) < 3*0.01*(p0_res[A1_BoardID][A1_ChID] * sqrt(abs(511.0-energy_S1)) / (2.0*sqrt(2.0*log(2.0))) + p0_res[S1_BoardID][S1_ChID] * sqrt(abs(energy_S1)) / (2.0*sqrt(2.0*log(2.0))))){
+                    if((energy_A1 > 50) && (energy_S1 > 50) && (energy_A1< 400) && (energy_S1 < 400)){
+                        if(abs(energy_A1 - (511.0 - energy_S1)) < 3*0.01*(p0_res[A1_BoardID][A1_ChID] * sqrt(abs(511.0-energy_S1)) / (2.0*sqrt(2.0*log(2.0))) + p0_res[S1_BoardID][S1_ChID] * sqrt(abs(energy_S1)) / (2.0*sqrt(2.0*log(2.0))))){
                         fH2Energy_PMTs->Fill(x_energy, y_energy);
                         fH1EnergySpectra[0]->Fill(x_energy);
                         fH1EnergySpectra[1]->Fill(y_energy);
                         fH1EnergySpectra[2]->Fill(x_energy+y_energy);
                         counter++;
+                        }
                     }
                 }
             }
@@ -3636,13 +3638,13 @@ Double_t DRS4Ana::Plot_2Dhist_energy_with_cut2(TString key = "0120", TString key
     fH2Energy_PMTs->Draw();
 
     //カット範囲の図示
-    TF1 *curve_upper = new TF1("error curve", error_curve_upper, 0.0, 511.0, 2);
-    curve_upper->SetParameters(y_p0_res_buf, x_p0_res_buf);
+    TF1 *curve_upper = new TF1("error curve", error_curve_upper, 0.0, 511.0, 4);
+    curve_upper->SetParameters(y_p0_res_buf, x_p0_res_buf,4);
     curve_upper->SetLineColor(kBlue);
     curve_upper->SetLineWidth(1);
     curve_upper->Draw("SAME");
-    TF1 *curve_lower = new TF1("error curve", error_curve_lower, 0.0, 511.0, 2);
-    curve_lower->SetParameters(y_p0_res_buf, x_p0_res_buf);
+    TF1 *curve_lower = new TF1("error curve", error_curve_lower, 0.0, 511.0, 4);
+    curve_lower->SetParameters(y_p0_res_buf, x_p0_res_buf,4);
     curve_lower->SetLineColor(kBlue);
     curve_lower->SetLineWidth(1);
     curve_lower->Draw("SAME");
