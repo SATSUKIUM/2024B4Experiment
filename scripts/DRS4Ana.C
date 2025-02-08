7646377dc4eada9d4dad590062137ab7065190b7
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-08 12:46:28 +0900

    cut2->draw cut curve

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -77,6 +77,13 @@ void listChains(const TString key = "456"){
     globalChain_Event->GetListOfFiles()->Print();
 }
 
+Double_t error_curve_upper(Double_t *x_energy, Double_t *p0_res){
+    return(511.0 - x_energy[0] + (p0_res[0] * sqrt(abs(511.0 - x_energy[0])) / (2.0*sqrt(2.0*log(2.0))) + p0_res[1] * sqrt(abs(x_energy[0])) / (2.0*sqrt(2.0*log(2.0))))); //p0_res[0]はyのp0_res、p0_res[1]はxのp0_res
+}
+Double_t error_curve_lower(Double_t *x_energy, Double_t *p0_res){
+    return(511.0 - x_energy[0] - (p0_res[0] * sqrt(abs(511.0 - x_energy[0])) / (2.0*sqrt(2.0*log(2.0))) + p0_res[1] * sqrt(abs(x_energy[0])) / (2.0*sqrt(2.0*log(2.0))))); //p0_res[0]はyのp0_res、p0_res[1]はxのp0_res
+}
+
 void DRS4Ana::PlotADCSum(Int_t iBoard, Int_t iCh)
 {
     gStyle->SetOptStat(0);
@@ -3672,9 +3679,4 @@ Double_t DRS4Ana::Plot_2Dhist_energy_with_cut2(TString key = "0120", TString key
     return counter;
 }
 
-Double_t error_curve_upper(Double_t *x_energy, Double_t *p0_res){
-    return(511.0 - x_energy[0] + (p0_res[0] * sqrt(abs(511.0 - x_energy[0])) / (2.0*sqrt(2.0*log(2.0))) + p0_res[1] * sqrt(abs(x_energy[0])) / (2.0*sqrt(2.0*log(2.0))))); //p0_res[0]はyのp0_res、p0_res[1]はxのp0_res
-}
-Double_t error_curve_lower(Double_t *x_energy, Double_t *p0_res){
-    return(511.0 - x_energy[0] - (p0_res[0] * sqrt(abs(511.0 - x_energy[0])) / (2.0*sqrt(2.0*log(2.0))) + p0_res[1] * sqrt(abs(x_energy[0])) / (2.0*sqrt(2.0*log(2.0))))); //p0_res[0]はyのp0_res、p0_res[1]はxのp0_res
-}
+
