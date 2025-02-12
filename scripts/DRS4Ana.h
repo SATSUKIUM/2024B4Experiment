71b7275c6aefd318b56d245eedcc738cbf59d366
Nirvana <yoshizawanaomichi111@gmail.com>
2025-02-12 22:04:27 +0900

    discritime difference

--- scripts/DRS4Ana.h
+++ scripts/DRS4Ana.h
@@ -157,7 +157,7 @@ public:
     virtual void Discricut2();
     virtual void Energy_fit(Int_t iBoard , Int_t ich , Int_t xMin=0, Int_t xMax=650, Int_t fitRangeMin=450 , Int_t fitRangeMax=580);
     virtual void PlotTrigger2();
-    virtual void PlotdiscriTime_difference(Int_t iBoard1 = 0, Int_t iCh1 = 0, Int_t iBoard2 = 0, Int_t iCh2 = 2, Int_t fit_flag = 1, Double_t fit_min = -5.0, Double_t fit_max = 25.0);
+    virtual void PlotdiscriTime_difference(Int_t iBoard1 = 0, Int_t iCh1 = 0, Int_t iBoard2 = 0, Int_t iCh2 = 2, Int_t fit_flag = 1, Double_t fit_min = -5.0, Double_t fit_max = 25.0, Double_t sigma = 2.0);
 
 
     TH2F *fH2Waveform = NULL;
