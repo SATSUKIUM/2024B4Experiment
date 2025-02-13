b40eb43c72c03381fa76d75478c5f3d9caa2f4a0
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-13 17:12:25 +0900

    adcSum

--- scripts/adcSum_crystals.C
+++ scripts/adcSum_crystals.C
@@ -105,6 +105,7 @@ void adcSum_crystals(TString filepath = "../data/PhysicsRun/ROOT_FILES/Run_005.d
                             }
                         }
                     }
+                    printf("\n\tDEBUG\n");
                 }
                 else{
                     discriTime = fTime[iBoard][iCh][fDiscriCell[iBoard][iCh]];
