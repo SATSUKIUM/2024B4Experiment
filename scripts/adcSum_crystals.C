f3fbd696d6acb19f353bd3fc3bce038304cb4e7e
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-13 17:24:17 +0900

    adcSum

--- scripts/adcSum_crystals.C
+++ scripts/adcSum_crystals.C
@@ -30,7 +30,7 @@
 #include <TChainElement.h>
 #include <TObjArray.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
 void adcSum_crystals(TString filepath = "../data/PhysicsRun/ROOT_FILES/Run_005.dat"){
     // 入力ファイルを開く
@@ -81,6 +81,7 @@ void adcSum_crystals(TString filepath = "../data/PhysicsRun/ROOT_FILES/Run_005.d
                     break;
                     }
                     pedeslta_sum += fWaveform[iBoard][iCh][iCell];
+                    counter++;
                     // if(DEBUG){
                     //     std::cout << fWaveform[iBoard][iCh][iCell] << std::endl;
                     // }
