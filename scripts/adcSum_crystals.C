1e073698d4f940b27ff5d6c72222a651878d6d8e
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-13 17:23:43 +0900

    adcSum

--- scripts/adcSum_crystals.C
+++ scripts/adcSum_crystals.C
@@ -81,9 +81,12 @@ void adcSum_crystals(TString filepath = "../data/PhysicsRun/ROOT_FILES/Run_005.d
                     break;
                     }
                     pedeslta_sum += fWaveform[iBoard][iCh][iCell];
-                    if(DEBUG){
-                        std::cout << fWaveform[iBoard][iCh][iCell] << std::endl;
-                    }
+                    // if(DEBUG){
+                    //     std::cout << fWaveform[iBoard][iCh][iCell] << std::endl;
+                    // }
+                }
+                if(DEBUG){
+                    std::cout << counter << std::endl;
                 }
                 pedeslta_sum = pedeslta_sum/counter;
                 if(iBoard == 0){
