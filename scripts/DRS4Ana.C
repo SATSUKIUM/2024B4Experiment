bde7792cdefcfc933e53edec915bafda96022942
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-14 15:51:07 +0900

    EventSelection2->using adcSum_crystals

--- scripts/DRS4Ana.C
+++ scripts/DRS4Ana.C
@@ -7282,3 +7282,306 @@ Double_t DRS4Ana::PlotEnergy2(TString key = "0204", Int_t iBoard = 0, Int_t iCh 
 
     return (Double_t)counter;
 }
+
+
+Double_t DRS4Ana::EventSelection2(TString key = "0204", Int_t x_iBoard = 0, Int_t x_iCh = 0, Int_t y_iBoard = 0, Int_t y_iCh = 1
+, bool applyTimeCut = true, bool applyScatterCut = true, bool applyEnergyCut = true){
+    fChain->SetBranchStatus("fSec",0);
+    fChain->SetBranchStatus("fNanoSec",0);
+    fChain->SetBranchStatus("fTriggerCell",0);
+    fChain->SetBranchStatus("adcSum",0);
+    
+    Long64_t nentries = fChain->GetEntriesFast();
+    Long64_t allcounter = 0;
+    Long64_t validcounter = 0;
+    Long64_t timecutcounter = 0;
+    Long64_t scattercutcounter = 0;
+
+    TCanvas *canvas = new TCanvas("canvas", "title", 2000, 4000);
+    canvas->Divide(2,2);
+    if(fH2Energy_PMTs != NULL){
+        delete fH2Energy_PMTs;
+    }
+    Double_t minEnergy, maxEnergy;
+    Int_t nBins = 100;
+    minEnergy = 0.0;
+    maxEnergy = 600.0;
+
+    TString key_Crystal_y;
+
+    Double_t p0[2][4], p0e[2][4], p1[2][4], p1e[2][4], p0_res[2][4], p0e_res[2][4];
+    Load_EnergycalbData(key, p0, p0e, p1, p1e, p0_res, p0e_res);
+
+    Double_t x_energy, y_energy, S1_energy, A1_energy;
+    Double_t x_error, y_error, S1_error, A1_error;
+    Double_t x_error_upper, x_error_lower;
+    Double_t x_charge_buf, y_charge_buf, S1_charge_buf, A1_charge_buf;
+    Double_t x_p0_buf, y_p0_buf, x_p1_buf, y_p1_buf;
+    Double_t y_upper, y_lower, A1_lower, A1_upper;
+
+    
+    x_p0_buf = p0[x_iBoard][x_iCh];
+    x_p1_buf = p1[x_iBoard][x_iCh];
+    y_p0_buf = p0[y_iBoard][y_iCh];
+    y_p1_buf = p1[y_iBoard][y_iCh];
+    
+    Double_t x_DiscriTime, y_DiscriTime, S1_DiscriTime, A1_DiscriTime;
+    Double_t x_DiscriCell, y_DiscriCell, S1_DiscriCell , A1_DiscriCell;
+    Double_t x_adcSum_timerange, y_adcSum_timerange;
+    Double_t x_p0_res_buf = p0_res[x_iBoard][x_iCh];
+    Double_t y_p0_res_buf = p0_res[y_iBoard][y_iCh];
+
+    Int_t TimeCut, ScatterCut, EnergyCut, AllCut;
+    bool cut0,cut1,cut2,cut3,cut4,cut5,cut6,cut7;
+    /*
+    cut0 : all DC > 0
+    cut1 : S1, A1, simultaneously
+    cut2 : S1, S1, simultaneously
+    cut3 : S2, A2_any deg, simultaneously
+    cut4 : 256 - 3sigma < E_S1 < 256 + 3sigma(これもっと強くして良いんじゃないか)
+    cut5 : 170 - 3sigma < E_S2 < 340 + 3sigma(これももっと強くして良いんじゃないか)
+    cut6 : 511 - 3(sqrt(sigma^2 + sigma^2)) < E_S2 + E_A2 < ::
+    cut7 : s1,a1 energy
+    */
+    bool TimeCutPassed, ScatterCutPassed, EnergyCutPassed;
+
+    x_adcSum_timerange = 600;
+   
+    if ((y_iBoard == 0 && y_iCh == 1) || (y_iBoard == 1)) {
+    y_adcSum_timerange = 180;
+    key_Crystal_y = "GSO";
+    }
+    else if (y_iBoard == 0 && y_iCh == 2) {
+    y_adcSum_timerange = 600; 
+    key_Crystal_y = "NaI";
+    }
+    else{
+        printf("\t\ny axis || type of crystal is invalid\n");
+    }
+
+
+    TH1D *fH1EnergySpectra[3];
+
+    fH1EnergySpectra[0] = new TH1D("fH1EnergySpectra", Form("x-axis energy spectrum : iBoard %d, iCh %d, crystal NaI", x_iBoard, x_iCh), nBins, minEnergy, maxEnergy);
+    fH1EnergySpectra[0]->SetTitle(Form("x-axis energy spectrum : iBoard %d, iCh %d, crystal NaI;energy [keV]; count per %.2f keV", x_iBoard, x_iCh, (maxEnergy-minEnergy)/nBins));
+    
+    fH1EnergySpectra[1] = new TH1D("fH1EnergySpectra", Form("y-axis energy spectrum : iBoard %d, iCh %d, crystal %s", y_iBoard, y_iCh, key_Crystal_y.Data()), nBins, minEnergy, maxEnergy);
+    fH1EnergySpectra[1]->SetTitle(Form("y-axis energy spectrum : iBoard %d, iCh %d, crystal %s;energy [keV]; count per %.2f keV", y_iBoard, y_iCh, key_Crystal_y.Data(), (maxEnergy-minEnergy)/nBins));
+    
+    
+    fH1EnergySpectra[2] = new TH1D("fH1EnergySpectra", "Sum energy spectrum", 100, 0, 600);
+    fH1EnergySpectra[2]->SetTitle(Form("Sum energy spectrum : iBoard %d, iCh %d, and iBoard %d, iCh %d;energy [keV]; count per %.2f keV", x_iBoard, x_iCh, y_iBoard, y_iCh,(maxEnergy-minEnergy)/nBins));
+
+    
+    fH2Energy_PMTs = new TH2F("name", "title", 200, -50, 600, 200, -50, 600);
+    fH2Energy_PMTs->SetTitle(Form("energy between two PMTs (data from cfg/%s/data.txt);Board%d Ch%d energy [keV];Board%d CH%d energy [keV]", key.Data(), x_iBoard, x_iCh, y_iBoard, y_iCh));
+    canvas->cd(1);
+    fH2Energy_PMTs->Draw();
+
+    gPad->SetGrid();
+    gPad->SetLogz();
+    gStyle->SetOptStat(0);
+
+    TF1 *curve_upper = new TF1("curve_upper", CurveUpper_ukai, 0.0, 511.0, 2);  // パラメータ数は 2
+    TF1 *curve_lower = new TF1("curve_lower", CurveLower_ukai, 0.0, 511.0, 2); 
+
+    for(Int_t Entry=0; Entry<nentries; Entry++){
+        fChain->GetEntry(Entry);
+
+        x_DiscriTime = fTime[x_iBoard][x_iCh][fDiscriCell[x_iBoard][x_iCh]];
+        y_DiscriTime = fTime[y_iBoard][y_iCh][fDiscriCell[y_iBoard][y_iCh]];
+        S1_DiscriTime = fTime[0][0][fDiscriCell[0][0]];
+        A1_DiscriTime = fTime[0][2][fDiscriCell[0][2]];
+        
+        x_DiscriCell = fDiscriCell[x_iBoard][x_iCh];
+        y_DiscriCell = fDiscriCell[y_iBoard][y_iCh];
+        S1_DiscriCell = fDiscriCell[0][0];
+        A1_DiscriCell = fDiscriCell[0][2];
+        
+        S1_charge_buf = -GetChargeIntegral(0, 0, 20, S1_DiscriTime - 50, S1_DiscriTime + 600);
+        A1_charge_buf = -GetChargeIntegral(0, 2, 20, A1_DiscriTime - 50, A1_DiscriTime + 600);
+
+        S1_energy = p0[0][0] + p1[0][0]* S1_charge_buf;
+        A1_energy = p0[0][2] + p1[0][2]* A1_charge_buf;
+        S1_error = p0_res[0][0] * sqrt(256) * 0.01 / (2 * sqrt(2 * log(2)));
+        A1_error = p0_res[0][2] * sqrt(A1_energy) * 0.01 / (2 * sqrt(2 * log(2)));
+
+        x_charge_buf = -GetChargeIntegral(x_iBoard, x_iCh, 20, x_DiscriTime - 50, x_DiscriTime + x_adcSum_timerange);
+        y_charge_buf = -GetChargeIntegral(y_iBoard, y_iCh, 20, y_DiscriTime - 50, y_DiscriTime + y_adcSum_timerange);
+
+        x_energy = x_p0_buf + x_p1_buf * x_charge_buf;
+        y_energy = y_p0_buf + y_p1_buf * y_charge_buf;
+
+        x_error = x_p0_res_buf * sqrt(x_energy) * 0.01 / (2 * sqrt(2 * log(2)));
+        y_error = y_p0_res_buf * sqrt(y_energy) * 0.01 / (2 * sqrt(2 * log(2)));
+          
+        x_error_upper = x_p0_res_buf * sqrt(256) * 0.01 / (2 * sqrt(2 * log(2)));
+        x_error_lower = x_p0_res_buf * sqrt(170) * 0.01 / (2 * sqrt(2 * log(2)));
+
+        A1_upper = -A1_energy + 3 * sqrt(pow(S1_error, 2) + pow(A1_error,2)) + 511;
+        A1_lower = -A1_energy - 3 * sqrt(pow(S1_error, 2) + pow(A1_error,2)) + 511;
+        y_upper = -x_energy + 3 * sqrt(pow(x_error, 2) + pow(y_error,2)) + 511;
+        y_lower = -x_energy - 3 * sqrt(pow(x_error, 2) + pow(y_error,2)) + 511;
+
+
+    // void ApplyCuts(bool applyTimeCut, bool applyScatterCut, bool applyEnergyCut) {
+
+    // bool applyTimeCut, applyScatterCut, applyEnergyCut;
+
+        cut0 =  (0 < S1_DiscriCell) && (0 < A1_DiscriCell) && (0 < x_DiscriCell) && (0 < y_DiscriCell);
+        cut1 =  (-17 <= S1_DiscriCell - A1_DiscriCell) && (S1_DiscriCell - A1_DiscriCell <= 22);
+        cut2 =  (-22 <= S1_DiscriCell - x_DiscriCell) && (S1_DiscriCell - x_DiscriCell <= 15);
+
+        cut3 = false;
+        if (y_iBoard == 0){
+            cut3 =  (-2 <= x_DiscriCell- y_DiscriCell) && (x_DiscriCell- y_DiscriCell <= 22);
+        }else if (y_iBoard == 1 && y_iCh == 0){
+            cut3 =  (6 <= x_DiscriCell- y_DiscriCell) && (x_DiscriCell- y_DiscriCell <= 34);
+
+        }else if (y_iBoard == 1 && y_iCh == 1){
+            cut3 =  (12 <= x_DiscriCell- y_DiscriCell) && (x_DiscriCell- y_DiscriCell <= 35);
+
+        }else if (y_iBoard == 1 && y_iCh == 2){
+            cut3 =  (10 <= x_DiscriCell- y_DiscriCell) && (x_DiscriCell- y_DiscriCell <= 39);
+
+        }else if (y_iBoard == 1 && y_iCh == 3){
+            cut3 =  (12 <= x_DiscriCell- y_DiscriCell) && (x_DiscriCell- y_DiscriCell <= 33);
+            }
+        
+
+        cut4 = ( 256 - 3 * S1_error < S1_energy ) && (S1_energy < 256 + 3 * S1_error);
+        cut5 = ( 170 - 3 * x_error_lower < x_energy ) && (x_energy < 340 + 3 * x_error_upper);
+
+    
+        cut6 = (y_lower <= y_energy && y_energy <= y_upper);
+        cut7 = (A1_lower <= A1_energy && A1_energy <= A1_upper);
+
+        TimeCutPassed = !applyTimeCut || (cut0 && cut1 && cut2 && cut3);
+        ScatterCutPassed = !applyScatterCut || (cut4 && cut5);
+        EnergyCutPassed = !applyEnergyCut || (cut6 && cut7);
+
+        // if (TimeCutPassed && ScatterCutPassed && EnergyCutPassed) {
+        // fH2Energy_PMTs->Fill(x_energy, y_energy);
+        // fH1EnergySpectra[0]->Fill(x_energy);
+        // fH1EnergySpectra[1]->Fill(y_energy);
+        // fH1EnergySpectra[2]->Fill(x_energy + y_energy);
+        // }
+        if (TimeCutPassed){
+            timecutcounter++;
+            if(ScatterCutPassed){
+                scattercutcounter++;
+            
+                if(EnergyCutPassed){
+                    validcounter++;
+                    fH2Energy_PMTs->Fill(x_energy, y_energy);
+                    fH1EnergySpectra[0]->Fill(x_energy);
+                    fH1EnergySpectra[1]->Fill(y_energy);
+                    fH1EnergySpectra[2]->Fill(x_energy + y_energy);
+                }
+            }
+            
+        }
+
+
+    // }
+    
+    // if((100 < y_DiscriTime && y_DiscriTime < 220) && ( A1_DiscriTime < 135)){
+
+        // if(( 256 - 3 * S1_error < S1_energy )&& (S1_energy < 256 + 3 * S1_error) && ( 170 - 3 * x_error_lower < x_energy ) && (x_energy < 340 + 3 * x_error_upper) ){
+        
+            //   if((y_lower <= y_energy && y_energy <= y_upper)){
+                // && ( 100 < A1_energy )
+                  
+                //   fH2Energy_PMTs->Fill(x_energy, y_energy);
+                //   fH1EnergySpectra[0]->Fill(x_energy);
+                //   fH1EnergySpectra[1]->Fill(y_energy);
+                //   fH1EnergySpectra[2]->Fill(x_energy + y_energy);
+
+                //   validcounter++;
+            
+            //    }
+            // energycutcounter++;
+            
+        // }
+        
+        //  timecutcounter++;
+        // }
+
+        if(Entry % 5000 == 0){
+            printf("\tPoint plot : %d\n", Entry);
+        }
+        allcounter++;
+
+    }
+
+
+// 各パッドに描画する
+
+// Pad1: 2Dヒストグラム
+    canvas->cd(1);
+    gPad->SetLeftMargin(0.15);  // 左の余白を広げる
+    gPad->SetGrid();
+    gStyle->SetPalette(kRainBow);
+    gPad->Update();
+    // gPad->SetBottomMargin(0.15);  // 下の余白を広げる
+    fH2Energy_PMTs->Draw();
+    TLine *line = new TLine(0, 511, 511,0);
+    line->SetLineColor(kBlack);
+    line->SetLineWidth(2);
+    line->Draw("SAME");
+
+    curve_upper->SetParameters(x_p0_res_buf, y_p0_res_buf); 
+    curve_upper->SetLineColor(kBlue);
+    curve_upper->Draw("SAME");
+
+    
+    curve_lower->SetParameters(x_p0_res_buf, y_p0_res_buf);  // par[0]=x_p0_res_buf, par[1]=y_p0_res_buf
+    curve_lower->SetLineColor(kBlue);
+    curve_lower->Draw("SAME");
+
+   
+// Pad2: y_energy ヒストグラム
+    canvas->cd(2);
+    gPad->SetLeftMargin(0.15);  // 左の余白を広げる
+    gPad->SetGrid();
+    fH1EnergySpectra[1]->Draw();
+
+// Pad3: x_energy ヒストグラム
+    canvas->cd(3);
+    gPad->SetLeftMargin(0.15);  // 左の余白を広げる
+    gPad->SetGrid();
+    fH1EnergySpectra[0]->Draw();
+
+// Pad4: x_energy + y_energy ヒストグラム
+    canvas->cd(4);
+    gPad->SetLeftMargin(0.15);  // 左の余白を広げる
+    gPad->SetGrid();
+    fH1EnergySpectra[2]->Draw();
+    // hs->Draw("hist stack");
+    // hs->GetHistogram()->Draw("axis same");
+    // gPad->Update();
+    // gPad->RedrawAxis();
+    //hs->GetXaxis()->SetRangeUser(0, 600);  // 必要な範囲に設定
+    //hs->GetYaxis()->SetRangeUser(0, 4000);
+   
+    canvas->Update();
+
+    //保存用のディレクトリを作る
+    TString folderPath = Makedir_Date();
+
+    TString filename_figure = fRootFile(fRootFile.Last('/')+1, fRootFile.Length()-fRootFile.Last('/'));
+    filename_figure.ReplaceAll(".", "_");
+    filename_figure += "_fH2Energy_PMTs.pdf";
+    printf("\n\tfigure saved as: %s/%s\n", folderPath.Data(), filename_figure.Data());
+
+    IfFile_duplication(folderPath, filename_figure);
+    canvas->SaveAs(Form("%s/%s", folderPath.Data(), filename_figure.Data()));
+    
+    
+    std::cout << "all events : " << allcounter << std::endl;
+    std::cout << "timecut events : " << timecutcounter << std::endl;
+    std::cout << "scattercut events : " << scattercutcounter << std::endl;
+    std::cout << "valid events :" << validcounter << std::endl;
+
+    return 0;
+}
