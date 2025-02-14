3cd8da5a5b3077b5d3c10e3863bcd65accc6d948
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-14 17:35:24 +0900

    ES2

---
+++ scripts/5GSOs_analysis_ES.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ROOTスクリプトを実行する関数
+run_root() {
+    XX=$1
+    YY=$2
+    root -l <<EOF
+.L DRS4Ana.C
+listChains("456")
+DRS4Ana obj
+obj.EventSelection2("0204", 0, 3, $XX, $YY)
+EOF
+}
+
+export -f run_root
+
+# 並列実行（XX, YY の組み合わせ）
+parallel run_root ::: 0 1 1 1 1 ::: 1 0 1 2 3
