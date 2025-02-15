15a6f53015552cb28a8215efb5cc3f68a38609d5
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-15 13:01:13 +0900

    NaI counters adcSum timerange miss

--- scripts/binary2tree_kashima2.C
+++ scripts/binary2tree_kashima2.C
@@ -561,7 +561,7 @@ int binary2tree_kashima2(const Char_t *binaryDataFile = "../data/test001.dat", c
                             if(time[iBoard][chID][iCell] >= discriTime - 50.0 && time[iBoard][chID][iCell] <= discriTime + 600.0){
                                 adcSum_crystals_buf += waveform[iBoard][chID][iCell] - pedeslta_sum;
                             }
-                            else if(time[iBoard][chID][iCell] > discriTime + 180.0){
+                            else if(time[iBoard][chID][iCell] > discriTime + 600.0){
                                 break;
                             }
                         }
