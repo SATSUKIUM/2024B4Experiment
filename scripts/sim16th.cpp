262675b2a24569dcb2588a8f9396b3e8c3d964ea
KASHIMASHI <u785588d@ecs.osaka-u.ac.jp>
2025-02-16 17:05:10 +0900

    sim16->evaluate error

--- scripts/sim16th.cpp
+++ scripts/sim16th.cpp
@@ -427,16 +427,17 @@ Double_t GetKappa(TString input_Folder = "./cfg/sim16_kappa/001.txt"){
     TGraphErrors* graph = new TGraphErrors();
 
     Int_t index_data = 0;
-    Double_t counts;
+    Double_t counts, EFFICIENCY;
 
-    while(ifs >> counts){
-        graph->SetPoint(index_data, index_data * 45, counts);
-        graph->SetPointError(index_data, 0, sqrt(counts));
+    while(ifs >> counts >> EFFICIENCY){
+        Double_t sigma_tatejiku = sqrt(pow(counts / (pow(EFFICIENCY, 2.0)), 2.0) * pow(sqrt(EFFICIENCY), 2.0) + pow(1 / EFFICIENCY, 2.0) * pow(sqrt(counts), 2.0));
+        graph->SetPoint(index_data, index_data * 45, counts/EFFICIENCY);
+        graph->SetPointError(index_data, 0, sigma_tatejiku);
         index_data++;
     }
     ifs.close();
 
-    TH2D* hh = new TH2D("h", "h", 10, -10, 190, 10, 0, 600); // 範囲変えて
+    TH2D* hh = new TH2D("h", "h", 10, -10, 190, 10, 0, 0.3); // 範囲変えて
     hh->SetStats(0);
     hh->SetTitle("The number of valid events;#phi[degree];Counts");
     hh->Draw();
